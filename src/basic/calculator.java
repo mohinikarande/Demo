0be26a35429c4738c9674f621c75a4cbda1f5cb5
mohinikarande <mohini.karande@worldline.com>
2022-08-18 17:38:33 +0530

    first comment

---
+++ src/basic/calculator.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class calculator {
+	public static void main(String args[])
+	{
+		System.out.println("Addition");
+		math.add(22,67);
+		math.add(65, 89, 43);
+		System.out.println("Multiplication");
+		math.multiply(23, 8);
+		}
+	}
